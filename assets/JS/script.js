a82b5936354888df4bdbc8faa610ebd4d6a56ee9
welersonfrr <contato.welerson@gmail.com>
2022-08-15 18:15:42 -0300

    Ajustes na mensagem e tamanhos

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -5,13 +5,15 @@ const link3 = document.getElementById("link-3");
 const link4 = document.getElementById("link-4");
 const link5 = document.getElementById("link-5");
 const tel = "+5545998261963";
-const wppUrl = "https://pastebin.com/raw/gePYD360";
 let storedText;
 
 phone.onclick = () => openUrl(`tel:${tel}`);
 link1.onclick = () => openUrl("https://www.linkedin.com/in/welerson-ferreira/");
 link2.onclick = () => openUrl("https://welersonfrr.github.io/curriculo/");
-link3.onclick = () => openUrl(`https://wa.me/${tel}?text=${wppUrl}`);
+link3.onclick = () =>
+  openUrl(
+    `https://wa.me/${tel}?text=Olá%20Welerson,%20tenho%20interesse%20em%20um%20site%20profissional!`
+  );
 link4.onclick = () => openUrl("https://www.tiktok.com/@welwrsonfrr");
 link5.onclick = () => openUrl("mailto:contato.welerson@gmail.com");
 
